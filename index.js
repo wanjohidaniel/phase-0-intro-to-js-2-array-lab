5de9bedb71678e04910a6df6ea3b70c770ae8976
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-25 21:11:50 -0700

    Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(); 
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+module.exports = destructivelyPrependCat;
+
+function removeFirstCat() {
+        if (cats.length === 0) {
+        return [];
+    }
+
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
+module.exports = removeFirstCat;
+
